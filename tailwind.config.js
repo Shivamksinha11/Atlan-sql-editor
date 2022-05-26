56d3248659eafec8b568038b53918653415d5e0f
shivamksinha11 <shivamksinha11@gmail.com>
2022-05-26 18:48:14 +0530

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat:['Montserrat', 'sans-serif'],
+        sans:['Open Sans', 'sans-serif'],
+        raleway:['Raleway', 'sans-serif'],
+        roboto:['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
